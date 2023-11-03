91539d229119b37d0fb45a84a79837bb29a5c251
Joseph <josefotieno6@gmail.com>
2023-11-03 01:20:47 -0700

    binary_trees

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -8,19 +8,24 @@
  *
  * Return: Sorted in descending order
  */
-int *heap_to_sorted_array(heap_t *heap, size_t *size) {
-    int *sorted_array;
-    size_t array_size = 0, i;
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	int *array;
+	int extract, i = 0;
+	size_t heap_size;
 
-    if (!heap || !size) return (NULL);
-    array_size = heap_size(heap);
-    sorted_array = malloc(sizeof(int) * array_size);
-    if (!sorted_array) return (NULL);
-
-    for (i = 0; i < array_size; i++) {
-        sorted_array[i] = heap->n;
-        heap_extract(&heap);
-    }
-    *size = array_size;
-    return (sorted_array);
+	if (!heap)
+		return (NULL);
+	heap_size = binary_tree_size(heap);
+	*size = heap_size;
+	array = malloc(heap_size * sizeof(int));
+	if (!array)
+		return (NULL);
+	while (heap)
+	{
+		extract = heap_extract(&heap);
+		array[i] = extract;
+		i++;
+	}
+	return (array);
 }
